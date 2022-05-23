41c1a5607a0e9f3478410fe12a10e7955268e19b
Akhila Reddy <Akhila Reddy@DESKTOP-2EKJHT2>
2022-05-23 10:46:58 +0530

    First Commit

---
+++ corejava/src/corejava_package/Day1_P3.java
@@ -0,0 +1,20 @@
+package corejava_package;
+abstract class atm
+{
+	abstract void withdraw();
+}
+
+public class Day1_P3  extends atm {
+	void withdraw()
+	{
+		System.out.println("withdraw successfully");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Day1_P3 ob1=new Day1_P3();
+        ob1.withdraw();
+        
+	}
+
+}
